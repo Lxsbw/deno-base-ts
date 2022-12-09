5c6b8c0e9e171841b0eaec8ea555e531a557ccaf
zhixiong <lxsbw@outlook.com>
2022-12-09 11:58:19 +0800

    update version & use denon scripts

---
+++ src/shell/app.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+appName=$1
+action=$2
+appPath=$3
+
+if [ ${action} = "start" ]; then
+  pm2 start ${appPath} --interpreter="deno" --interpreter-args="run --allow-net" --name=${appName}
+else
+  pm2 stop ${appName}
+  pm2 delete ${appName}
+fi;
