02dc3b2b28788079768918f1317d54def88577fd
zhixiong <lxsbw@outlook.com>
2023-03-15 14:50:45 +0800

    allow-read

--- src/shell/app.sh
+++ src/shell/app.sh
@@ -5,7 +5,7 @@ action=$2
 appPath=$3
 
 if [ ${action} = "start" ]; then
-  pm2 start ${appPath} --interpreter="deno" --interpreter-args="run --allow-net" --name=${appName}
+  pm2 start ${appPath} --interpreter="deno" --interpreter-args="run --allow-net --allow-read" --name=${appName}
 else
   pm2 stop ${appName}
   pm2 delete ${appName}
--- src/shell/appX.sh
+++ src/shell/appX.sh
@@ -5,7 +5,7 @@ action=$2
 appPath=$3
 
 if [ ${action} = "start" ]; then
-  pm2 start ${appPath} --interpreter="deno" --interpreter-args="run --allow-net --importmap=import_map.json --unstable" --name=${appName}
+  pm2 start ${appPath} --interpreter="deno" --interpreter-args="run --allow-net --allow-read --importmap=import_map.json --unstable" --name=${appName}
 else
   pm2 stop ${appName}
   pm2 delete ${appName}
